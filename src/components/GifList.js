4f0803178f921aa6b99eadbe6758bd3876d0c196
Taci Shlosberg <tshlosberg@gmail.com>
2020-09-16 16:37:40 -0700

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,13 +1,19 @@
 import React from 'react'
 
 function GifList(props) {
-    let gifArray = props.gifs.map((gifObj) => <li><img src={gifObj.images.original.url} /></li>)
+
+    let gifArray = props.gifs.map((gifObj) => 
+    <li><img src={gifObj.images.original.url} /></li>)
     console.log("gif array: ", gifArray)
     return (
         <ul>
             {gifArray}
         </ul>
     )
+
+    fetch('https://api.giphy.com/v1/gifs/search?q=YOUR QUERY HERE&api_key=dc6zaTOxFJmzC&rating=g')
+    .then(resp => resp.json())
+    .then(data => this.setState({d: data}))
 }
 
 export default GifList
